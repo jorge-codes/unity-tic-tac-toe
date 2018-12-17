fae13a09d0f88c2a1c2b8e91d3925f4dec238968
Jorge Palacios <pctroll@gmail.com>
2018-12-17 20:12:21 -0300

    Basic UI layout done

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,47 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// 
 /// </summary>
 public class GameManager : MonoBehaviour
 {
+    [Header("References")]
+    
+    public Text txtPointsWin;
+    public Text txtPointsTie;
+    public Text txtPointsLose;
+    public Button[] btnBoard;
+    
+    [Header("Member variables")]
+    public int pointsWin;
+    public int pointsTie;
+    public int pointsLose;
+
+    private int[] _board;
+
+    
+    public void Play(int playerId, int boardPositon)
+    {
+        _board[boardPositon] = playerId;
+    }
+
+    private void Start()
+    {
+        pointsWin = pointsTie = pointsLose = 0;
+        UpdateScore();
+
+        _board = new int[9];
+        
+    }
+
+    private void UpdateScore()
+    {
+        txtPointsLose.text = pointsLose.ToString();
+        txtPointsTie.text = pointsTie.ToString();
+        txtPointsWin.text = pointsWin.ToString();
+    }
+    
+
     
 }
